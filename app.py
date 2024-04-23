40795b7c0fb4b4507a964a4cb745012e5f36c7cf
yujin2505 <167831858+yujin2505@users.noreply.github.com>
2024-04-23 13:05:31 +0900

    1
    1

---
+++ app.py
@@ -0,0 +1,11 @@
+# 스트림릿 라이브러리를 사용하기 위한 임포트
+import streamlit as st
+
+# 웹 대시보드 개발 라이브러리인 스트림릿은 main함수가 있어야 한다
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
+
---
+++ app2.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main() :
+    #텍스트를 표시하는 방법
+    st.title('웹 대시보드')
+    st.text('웹 대시보드 개발하기')
+
+if __name__ == '__main__' :
+    main()
