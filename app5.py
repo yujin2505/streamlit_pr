a6a547ab0a2e2e5815e806d621483659bef270b0
yujin2505 <167831858+yujin2505@users.noreply.github.com>
2024-04-23 16:12:19 +0900

    yyy
    yyy

--- app5.py
+++ app5.py
@@ -11,15 +11,21 @@ def main():
     
     st.image(img)
     st.image(img, width=500)
-    st.image(img, width=True) #width=True해주면 자동으로 폭을 맞춰준다
+    st.image(img, use_column_width=True) #width=True해주면 자동으로 폭을 맞춰준다
 
     #2. 인터넷상에 있는 이미지를 화면에 표시하는 방법 인터넷상의 이미지:URL이 있다
     
-    url = 'https://i.imgur.com/QgWbK9D.gif'
+    #사진 파일
+    url = 'https://miro.medium.com/v2/resize:fit:1000/format:webp/1*_ruPoPC4tiUP9Ojtiec4nw.gif'
     st.image(url)
     
+    #동영상 파일
     video_file = open('./data/video1.mp4','rb')
     st.video(video_file)
+    
+    #오디오 파일
+    audio_file = open('./data/song.mp3', 'rb')
+    st.audio(audio_file.read(), format='audio/mp3')
     
 if __name__ == '__main__':
     main()
